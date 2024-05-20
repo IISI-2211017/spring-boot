a0f23bd3755eef7a3be66fe15cc39c330c1dce4a
tuna <2211017@iisiGroup.com>
2024-05-20 12:56:47 +0800

    Revert "測試"
    This reverts commit a586fe7d7c04fd91957a2253b9bcdcb7de39ed48.

--- src/main/java/com/Application.java
+++ src/main/java/com/Application.java
@@ -1,8 +1,5 @@
 package com;
 
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -12,11 +9,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 //@ComponentScan(basePackages = "com.jpa")
 public class Application {
 
-	private static final Logger logger = LoggerFactory.getLogger(Application.class);
-	
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
-		logger.info("Debug log message");
 	}
 	
 }
